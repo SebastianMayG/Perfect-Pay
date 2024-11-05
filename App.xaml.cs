1721c20e7553a469da73de7bb1442822add86158
SebastianMayG <al064488@uacam.mx>
2024-11-04 18:35:16 -0600

    :art: Diseño de la interfaz de usuario

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Perfect_Pay
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Perfect_Pay
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
